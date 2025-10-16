94cf597eb2defb7ae060e729211f91f73c7ac7dc
Ibnushemsudin <musteshemsu@gmail.com>
2025-10-16 13:29:16 -0700

    Refactor App component to include ChatInput and manage chat messages state

--- ChatBot-project/src/App.jsx
+++ ChatBot-project/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-
+import Chatinput from './Components/ChatInput'
 function App() {
+  const [chatMessages, setChatMessages] = useState([]);
+
   return (
-    <> 
-           
+    <> <Chatinput chatMessages={chatMessages} setChatMessages={setChatMessages} />
     </>
   )
 }
